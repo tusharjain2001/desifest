473d8d10ea45ac37bbb37ae087c6943c027c20d2
David-2610 <davidtembhare1005@gmail.com>
2026-01-22 15:28:22 +0530

    yes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  css: {
+    transformer: "postcss", 
+  },
+});
