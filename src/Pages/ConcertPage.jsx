d1e546aa9332dfa5eb744717e8a9eea55dca50b7
David-2610 <davidtembhare1005@gmail.com>
2026-01-25 15:29:37 +0530

    BACKGROUND

--- src/Pages/ConcertPage.jsx
+++ src/Pages/ConcertPage.jsx
@@ -1,16 +1,33 @@
 import ArtistSignUp from "@/Components/Concerts/ArtistSignUp";
 import Hero from "@/Components/Concerts/Hero";
 import Volunteer2026 from "@/Components/Concerts/Volunteer2026";
+import backgroung1 from "@/Assets/home/background_1.png";
 import React from "react";
 
 const ConcertPage = () => {
-  return (
-    <div className="ml-24">
-      <Hero />
-      <ArtistSignUp />
-      <Volunteer2026 />
-    </div>
-  );
+	return (
+		<div className="relative min-h-screen w-full overflow-hidden">
+			{/* Background */}
+			<img
+				src={backgroung1}
+				alt="background"
+				className="absolute inset-0 w-full h-screen object-cover opacity-[0.16]"
+			/>
+			<div
+				className="absolute z-10 inset-0"
+				style={{
+					background:
+						"linear-gradient(270deg , #000000 -0.34%, rgba(0, 0, 0, 0) 56.45%)",
+				}}
+			/>
+			{/* Page Content */}
+			<div className="relative z-10 ml-24">
+				<Hero />
+				<ArtistSignUp />
+				<Volunteer2026 />
+			</div>
+		</div>
+	);
 };
 
 export default ConcertPage;
