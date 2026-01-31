0e69cb8f1f7558002dcc5e27b0419a3b384cc98c
David-2610 <davidtembhare1005@gmail.com>
2026-02-01 00:36:49 +0530

    chnages

--- src/Pages/PressKitPage.jsx
+++ src/Pages/PressKitPage.jsx
@@ -1,7 +1,15 @@
+import TestimonialCarousel from "@/Components/About/Testimonial";
+import MissionStatement from "@/Components/Media/MissionStatement";
+import OfficialLogos from "@/Components/Media/OfficialLogos";
+import PeopleOfContact from "@/Components/Media/PeopleOfContact";
+
 const PressKitPage = () => {
   return (
-    <div>PressKitPage</div>
+    <div>
+      <MissionStatement />
+      <PeopleOfContact />
+      <OfficialLogos />
+      <TestimonialCarousel />
+    </div>
   );
 };
-
-export default PressKitPage;
