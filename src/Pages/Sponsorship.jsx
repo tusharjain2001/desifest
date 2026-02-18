900a75d2d19891d1b1685a98cacb6e35b1fa0d0c
Tushar Jain <tushar.j@rrhaitech.com>
2026-02-18 17:35:54 +0530

    added youtube

--- src/Pages/Sponsorship.jsx
+++ src/Pages/Sponsorship.jsx
@@ -1,10 +1,12 @@
 import ContactForm from '@/Components/Home/ContactForm'
 import Calendar from '@/Components/Sponsorship/Calendar'
+import YouTube from '@/Components/Sponsorship/YouTube'
 import React from 'react'
 
 const Sponsorship = () => {
     return (
         <div>
+            <YouTube />
             <Calendar />
             <ContactForm />
         </div>
