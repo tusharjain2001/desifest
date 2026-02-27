8dc16dc4bf7998259105ef10c1e572d037813523
David-2610 <davidtembhare1005@gmail.com>
2026-02-27 16:45:32 +0530

    SECtion aDDED

--- src/Components/SofaSession/SecondSection.jsx
+++ src/Components/SofaSession/SecondSection.jsx
@@ -1,9 +1,10 @@
 import arrow from '@/Assets/home/animation/image.png'
 import { useEffect, useRef } from 'react'
+import image1 from '@/Assets/home/background_2.png'
+import image2 from '@/Assets/openmic/image copy 4.png'
 // IMAGES
 
 import img2 from '@/Assets/sofa_session/sofasessionsecondsection.svg'
-import bg from '@/Assets/sofa_session/sofasessionsecondsectionbg.png'
 
 const ArtistCard = ({ image, title, description }) => {
     return (
@@ -14,7 +15,7 @@ const ArtistCard = ({ image, title, description }) => {
                 <img
                     src={image}
                     alt={title}
-                    className="h-60 w-3/5 group-hover:w-4/5 object-cover  transition-all duration-500 ease-out "
+                    className="h-60 w-3/5 object-cover transition-all duration-500 ease-out group-hover:w-4/5"
                 />
             </div>
 
@@ -63,13 +64,9 @@ const SecondSection = ({ scrollY }) => {
     }, [scrollY])
 
     return (
-        <section className="relative w-full overflow-x-hidden overflow-y-clip bg-[#AC48FF] py-16 lg:py-32">
+        <section className="relative w-full overflow-x-hidden overflow-y-clip bg-[#AC48FF]">
             <div className="md:px-6 md:pl-40">
                 {/* DESKTOP BG */}
-                <img
-                    src={bg}
-                    className="pointer-events-none absolute -bottom-10 hidden h-200 lg:block"
-                />
 
                 {/* GRID CONTAINER (UNCHANGED) */}
                 <div className="relative z-30 mx-auto grid w-full max-w-full grid-cols-1 gap-16 px-6 pt-20 lg:grid-cols-3">
@@ -100,11 +97,48 @@ const SecondSection = ({ scrollY }) => {
                         {/* BG attached to 3rd card */}
                     </div>{' '}
                 </div>
-                <div className="relative h-100">
-                    <img
-                        src={bg}
-                        className="pointer-events-none absolute left-10 scale-150 md:hidden"
-                    />
+            </div>
+            {/* bottom image */}
+            <div className="ml-24 flex w-full items-center justify-center sm:pb-40">
+                <div className="relative flex min-h-screen w-full items-center justify-center">
+                    {/* Gradient Blob */}
+                    <div className="absolute h-60 w-60  -translate-x-28  translate-y-10 sm:translate-y-20 sm:-translate-x-16 sm:h-80   sm:w-80 ">
+                        {/* Circle Background */}
+                        <div className="absolute inset-0 -rotate-30 rounded-full bg-[linear-gradient(180deg,_#DDFE08_0%,_#541FDF_100%)]"></div>
+
+                        {/* Circular Text */}
+                        <div className="absolute inset-0 jersey flex sm:translate-y-0 -translate-y-8 sm:-translate-x-4 items-center justify-center rotate-0">
+  {'LET’S LIVE!!'.split('').map((char, index) => {
+    const total = 13
+    const spread = 180
+    const rotation = spread / 2 - (spread / total) * index
+
+    return (
+      <span
+        key={index}
+        className="absolute left-1/2 top-1/2 origin-top text-5xl sm:text-8xl font-bold sm:tracking-widest tracking-wide text-white"
+        style={{
+          transform: `rotate(${rotation}deg) rotate(${rotation/44}deg)  translateY(160px) `,
+        }}
+      >
+        {char}
+      </span>
+    )
+  })}
+</div>
+                    </div>
+                    {/* Left Circle */}
+                    <div className="absolute h-80 w-80   -translate-y-40  -translate-x-46   sm:-translate-y-64  sm:-translate-x-60  z-20 overflow-hidden rounded-full border-8 border-white sm:h-140 sm:w-140">
+                        <img src={image1} alt="Live Band" className="h-full w-full object-cover" />
+                    </div>
+                    {/* Right Circle */}
+                    <div className="absolute h-80 w-80   -translate-y-4 translate-x-16  sm:-translate-y-36  sm:translate-x-60 z-10 overflow-hidden rounded-full sm:h-140 sm:w-140">
+                        <img
+                            src={image2}
+                            alt="Live Singer"
+                            className="h-full w-full object-cover"
+                        />
+                    </div>
                 </div>
             </div>
         </section>
